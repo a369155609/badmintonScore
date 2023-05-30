cc2d573772f861dca07db84c3296b579636d5a59
a369155609 <1985055390@qq.com>
2023-05-30 18:40:35 +0800

    首页布局提交

--- app/src/main/java/com/junyu/badmintonscore/MainActivity.kt
+++ app/src/main/java/com/junyu/badmintonscore/MainActivity.kt
@@ -7,41 +7,60 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material.Button
+import androidx.compose.material.BackdropScaffold
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Navigation
+import androidx.compose.material.icons.filled.PlayCircle
+import androidx.compose.material.icons.filled.SportsEsports
+import androidx.compose.material.icons.filled.SportsTennis
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.junyu.badmintonscore.ui.base.CraneDrawer
 
 import com.junyu.badmintonscore.viewmodel.MainViewModel
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     val mainViewModel by lazy {
         ViewModelProvider(this)[MainViewModel::class.java]
     }
+
+    val labelList = listOf("首页","打球","比赛","我的")
+
+    val iconList = listOf(Icons.Default.Home,Icons.Default.SportsTennis,Icons.Default.Navigation,Icons.Default.Info)
 
     val text = buildAnnotatedString {
         withStyle(
@@ -62,9 +81,9 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @Preview(showBackground = true)
+
     @Composable
-    private fun testCompose() {
+    private fun TestCompose(contentPadding: PaddingValues) {
         Box(
             modifier = Modifier.fillMaxSize(),
 
@@ -102,26 +121,90 @@ class MainActivity : ComponentActivity() {
                 Text(text = text)
             }
 
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+//                    .clip(CircleShape)
+                    .background(Color(R.color.purple_200))
+                    .offset(x = 20.dp, y = 30.dp)
+
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.SportsTennis,
+                    contentDescription = "Favorite",
+                )
+            }
+
         }
 
     }
-
-
-}
-
-@Composable
-fun Greeting() {
-    val scaffoldState = rememberScaffoldState()
-    Scaffold(
-        scaffoldState = scaffoldState,
-        modifier = Modifier.statusBarsPadding(),
-        drawerContent = {
-            CraneDrawer()
+    @Composable
+    fun Greeting() {
+        val scaffoldState = rememberScaffoldState()
+        var selectIndex = remember{
+            mutableStateOf(0)
         }
-    ) { contentPadding ->
-        val scope = rememberCoroutineScope()
-        Text(text = "测试", modifier = Modifier.padding(contentPadding))
+
+        Scaffold(
+            scaffoldState = scaffoldState,
+            modifier = Modifier.statusBarsPadding(),
+            drawerContent = {
+                CraneDrawer()
+            }, bottomBar = {
+                CreateBottomBar(selectIndex)
+            }
+        ) { contentPadding ->
+            val scope = rememberCoroutineScope()
+//        Text(text = "测试", modifier = Modifier.padding(contentPadding))
+//            TestCompose(contentPadding)
+            pageChange(contentPadding,selectIndex)
+        }
+
+    }
+    @Composable
+    private fun pageChange(contentPadding: PaddingValues, selectIndex: MutableState<Int>) {
+        val count by remember { mutableStateOf(0) }
+
+        when(selectIndex.value){
+            0 ->{
+                TextPage("首页",contentPadding)
+            }
+            1 ->{
+                TextPage("打球",contentPadding)
+            }
+            2 ->{
+                TextPage("比赛",contentPadding)
+            }
+            3 ->{
+                TextPage("我的",contentPadding)
+            }
+
+        }
+    }
+    @Composable
+    private fun TextPage(s: String,contentPadding: PaddingValues,) {
+        Box(modifier = Modifier.fillMaxSize().padding(contentPadding)) {
+            Text(text = s,modifier = Modifier.align(Alignment.Center))
+        }
     }
 
+    @Composable
+    fun CreateBottomBar(selectIndex: MutableState<Int>) {
+        BottomAppBar(backgroundColor = colorResource(id = R.color.white)) {
+            labelList.forEachIndexed{
+                index, item ->
+                BottomNavigationItem(label = { Text(text = item)},
+                    selected = index == selectIndex.value ,
+                    onClick = { selectIndex.value = index }, icon = { Icon(
+                        imageVector = iconList[index],
+                        contentDescription = null
+                    )}, selectedContentColor = colorResource(id = R.color.teal_200), unselectedContentColor = colorResource(id = R.color.purple_200))
+            }
+            
+        }
+    }
 }
+
+
 
